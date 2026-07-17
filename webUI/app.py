16ade8499961846aa3c76e4c7e52377b5be4647e
Sandeep-pujari <sandeeppujari086@gmail.com>
2026-07-17 20:56:52 +0530

    update

--- webUI/app.py
+++ webUI/app.py
@@ -216,4 +216,9 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    import os
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False
+    )
